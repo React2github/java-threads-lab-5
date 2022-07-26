422b1264da8c7e99cf9363ab8dd602b391d6718c
Abdella <abdella.shukur2@gmail.com>
2022-07-26 14:03:44 -0400

    withdrawl working for project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
 public class Main {
-    public static void main(String[] args) {
-        // your code here
+    public static void main(String[] args) throws InterruptedException {
+
+        SavingsAccount savingsAccount = new SavingsAccount();
+
+        Thread t1 = new Thread(() -> savingsAccount.deposit(100));
+        Thread t2 = new Thread(() -> savingsAccount.withdraw(20));
+        Thread t3 = new Thread(() -> savingsAccount.deposit(30));
+
+        t1.start();
+        t1.join();
+        t2.start();
+        t2.join();
+        t3.start();
+        t3.join();
+
+        System.out.println(savingsAccount.getTotal());
+
     }
 }
--- src/main/java/SavingsAccount.java
+++ src/main/java/SavingsAccount.java
@@ -3,14 +3,21 @@ public class SavingsAccount {
   private long total = 0;
 
   public boolean withdraw(long amount) {
-      
+    if(total < amount) {
+      System.out.println("Can't withdraw. Insufficient funds");
+      return false;
+    } else {
+      System.out.println("Please wait. Dispensing cash");
+      total -= amount;
+      return true;
+    }
   }
 
   public void deposit(long amount) {
-
+    total += amount;
   }
 
   public long getTotal() {
-      return total;
+    return total;
   }
 }

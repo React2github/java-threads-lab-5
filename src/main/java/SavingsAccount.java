c268de12379500d70a65138c478e953398b11ed8
Abdella <abdella.shukur2@gmail.com>
2022-07-28 21:39:57 -0400

    updateS

--- src/main/java/SavingsAccount.java
+++ src/main/java/SavingsAccount.java
@@ -3,21 +3,19 @@ public class SavingsAccount {
   private long total = 0;
 
   public synchronized boolean withdraw(long amount) {
-    if(total < amount) {
-      System.out.println("Can't withdraw. Insufficient funds");
-      System.out.println(total + " if total less than amount");
-      return false;
-    } else {
-      System.out.println("Please wait. Dispensing cash");
-      total -= amount;
-      System.out.println(total + " if total is greater than amount");
+    // checks for money
+    if(total - amount >= 0) {
+      System.out.println("Please wait. Dispensing cash.");
+      total = total - amount;
       return true;
+    } else {
+      System.out.println("Insufficient funds. Goodbye");
+      return false;
     }
   }
 
   public synchronized void deposit(long amount) {
-    total += amount;
-    System.out.println(total + "  deposit amount");
+    total = total + amount;
   }
 
   public synchronized long getTotal() {
